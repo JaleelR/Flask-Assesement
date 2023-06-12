a8aa8d224d42e4f29e431ac604c9e8da00f82067
Jaleel Whaley <jw430118@gmail.com>
2023-06-12 18:00:49 -0400

    added more tests

--- test_app.py
+++ test_app.py
@@ -10,28 +10,26 @@ class viewFunctionsTestCase(TestCase):
         self.assertEqual(resp.status_code, 200)
         self.assertIn('\n<h1>Converter!</h1>\n', html)
 
+
     def test_uppercase(self):
         with app.test_client() as client:
-            resp = client.post("/convert",
-            data = {
-               "from": "usd", 
-               "to": "USD",
-               "amount": "1200"
-            })
+            resp = client.post("/convert")
         html = resp.get_data(as_text=True)
         self.assertEqual(resp.status_code, 302)
         
     
     def test_incorrect_currency_code(self):
         with app.test_client() as client:
-            resp = client.post("/convert",
+            resp = client.post("/convert", 
             data = {
                "from": "USD", 
                "to": "ZZZ",
                "amount": "1200"
-            })
+            }, follow_redirects= True)
         html = resp.get_data(as_text=True)
-        self.assertEqual(resp.status_code, 302)
+
+        self.assertEqual(resp.status_code, 200)
+        self.assertIn( "ZZZ is not a valid currency, some examples of currency codes would be" , html )
       
 
     def test_redirection(self):
@@ -41,6 +39,6 @@ class viewFunctionsTestCase(TestCase):
             self.assertEqual(resp.status_code, 302)
             self.assertEqual(resp.location, "http://localhost/")
 
-
+    
     
 
